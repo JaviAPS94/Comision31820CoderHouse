4b895eda1704681c0ff27453c7ef484581534a2e
Alex Pinaida <alex.pinaida@rappi.com>
2022-04-09 14:04:29 -0500

    [coderhouse] last

---
+++ Clase6/entregable/contenedores/ContenedorMemoria.js
@@ -0,0 +1,26 @@
+class ContenedorMemoria {
+    constructor() {
+        this.elementos = []
+        this.id = 0
+    }
+
+    listar(id) {
+    }
+
+    listarAll() {
+    }
+
+    guardar(elem) {
+    }
+
+    actualizar(elem, id) {
+    }
+
+    borrar(id) {
+    }
+
+    borrarAll() {
+    }
+}
+
+module.exports = ContenedorMemoria
---
+++ Clase6/entregable/public/js/main.js
@@ -0,0 +1,41 @@
+const socket = io.connect();
+
+//------------------------------------------------------------------------------------
+
+const formAgregarProducto = document.getElementById('formAgregarProducto')
+formAgregarProducto.addEventListener('submit', e => {
+    
+})
+
+socket.on('productos', productos => {
+    
+});
+
+function makeHtmlTable(productos) {
+    return fetch('plantillas/tabla-productos.hbs')
+        .then(respuesta => respuesta.text())
+        .then(plantilla => {
+            const template = Handlebars.compile(plantilla);
+            const html = template({ productos })
+            return html
+        })
+}
+
+//-------------------------------------------------------------------------------------
+
+const inputUsername = document.getElementById('inputUsername')
+const inputMensaje = document.getElementById('inputMensaje')
+const btnEnviar = document.getElementById('btnEnviar')
+
+const formPublicarMensaje = document.getElementById('formPublicarMensaje')
+formPublicarMensaje.addEventListener('submit', e => {
+
+})
+
+socket.on('mensajes', mensajes => {
+
+})
+
+function makeHtmlList(mensajes) {
+
+}
